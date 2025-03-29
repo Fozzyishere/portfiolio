8b6b374d5259c7b42df7f60561fce52e073b6550
Fozzy Is Here <danielpham13092002@gmail.com>
2025-03-29 14:41:36 +1100

    adjust navbar

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className="bg-gray-100 py-20">
       <div className="container mx-auto text-left">
-        <h1 className="text-4xl font-bold mb-4 text-blue-400">
+        <h1 className="text-5xl font-bold mb-4 text-blue-400">
           <Typewriter string="Quoc Huy Pham" typeSpeed={100} />
         </h1>
         <p className="text-l text-gray-600 mb-8">
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,8 +5,12 @@ const Navbar = () => {
     <nav>
       <div className="bg-white text-blue-700 flex flex-wrap items-center mx-auto p-4">
         <ul className="ml-auto flex flex-row">
-          <li className="p-2">About</li>
-          <li className="p-2">Projects</li>
+          <li className="mr-6 hover:text-blue-800">
+            <a href="#projects">Projects</a>
+          </li>
+          <li className="mr-6 hover:text-blue-800">
+            <a href="#contact">Contact</a>
+          </li>
         </ul>
       </div>
     </nav>
