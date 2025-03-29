6ebf0309fca0b4a6b09bc160ee0ec5cfa5fb28f6
Fozzy is Here <danielpham13092002@gmail.com>
2025-03-29 12:33:55 +1100

    hero updated with type writer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+'use client'
+
 import React from 'react';
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+
 export default function Home() {
   return (
     <main className="bg-gray-100 flex flex-col min-h-screen p-24">
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Typewriter from "./Typewriter";
 
 const Hero = () => {
   return (
     <div className="bg-gray-100 py-20">
       <div className="container mx-auto text-left">
-        <h1 className="text-5xl font-bold text-gray-800 mb-4">
-          Quoc Huy Pham
+        <h1 className="text-4xl font-bold mb-4 text-blue-400">
+          <Typewriter string="Quoc Huy Pham" typeSpeed={100} />
         </h1>
         <p className="text-l text-gray-600 mb-8">
           A guy trying to learn and build things.
