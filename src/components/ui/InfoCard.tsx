42e435bb703c7beed8e70809c371f1b153484b74
Fozzy Is Here <danielpham13092002@gmail.com>
2025-03-31 23:45:09 +1100

    fix: adjust cards

--- src/components/ui/InfoCard.tsx
+++ src/components/ui/InfoCard.tsx
@@ -6,8 +6,6 @@ interface InfoCardProps {
   //Next image properties
   src: StaticImageData;
   alt: string;
-  width: number;
-  height: number;
   //Text properties
   descText: string;
   techUsedText: string[];
@@ -16,39 +14,33 @@ interface InfoCardProps {
 const InfoCard: React.FC<InfoCardProps> = ({
   src,
   alt,
-  width,
-  height,
   descText,
   techUsedText,
 }) => {
   return (
-    <Card className="w-3xl flex flex-row">
-      <div className="items-center justify-start">
-        <Image
-          src={src}
-          alt={alt}
-          width={width}
-          height={height}
-          className="rounded-sm shadow-lg object-contain"
-        />
-      </div>
-      <div className="ml-4 flex flex-col">
-        <h2 className="text-xl font-semibold text-gruvbox-purple-dark mb-2">
-          {alt}
-        </h2>
-        <p className="text-gruvbox-fg2 text-sm">{descText}</p>
-        <div className="mt-3 flex gap-2">
-          {techUsedText.map((tech) => (
-            <span
-              key={tech}
-              className="px-2 py-1 bg-gruvbox-bg2 rounded-md text-xs"
-            >
-              {tech}
-            </span>
-          ))}
+    <div className="mt-10 flex flex-row items-center justify-center w-full mx-5">
+      <Card className="flex flex-row w-2/3">
+        <div className="relative overflow-hidden rounded-sm">
+          <Image src={src} alt={alt} width={500} height={500} />
         </div>
-      </div>
-    </Card>
+        <div className="ml-4 flex flex-col w-3/4 h-48">
+          <h2 className="text-xl font-semibold text-gruvbox-purple-dark mb-2 mt-4">
+            {alt}
+          </h2>
+          <p className="text-gruvbox-fg2 text-sm">{descText}</p>
+          <div className="mt-3 flex gap-2">
+            {techUsedText.map((tech) => (
+              <span
+                key={tech}
+                className="px-2 py-1 bg-gruvbox-bg2 rounded-md text-xs"
+              >
+                {tech}
+              </span>
+            ))}
+          </div>
+        </div>
+      </Card>
+    </div>
   );
 };
 
